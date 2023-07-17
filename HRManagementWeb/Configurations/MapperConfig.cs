a6478e32902612f8643c1832569ed8bd5d3d7209
Craig Yeoman <yeomancraig@outlook.com>
2023-07-17 15:04:58 -0500

    Modified the crud layouts and added datatables and swal.

---
+++ HRManagementWeb/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HRManagementWeb.Data;
+using HRManagementWeb.Models;
+
+namespace HRManagementWeb.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
