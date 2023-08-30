15e76b4405260e56635550ff76101c06f8825a56
Craig Yeoman <yeomancraig@outlook.com>
2023-08-30 06:36:31 -0500

    Minor CSS changes

--- HRManagementData/Configurations/Entities/UserSeedConfiguration.cs
+++ HRManagementData/Configurations/Entities/UserSeedConfiguration.cs
@@ -25,12 +25,108 @@ namespace HRManagementData.Configurations.Entities
                  new Employee
                  {
                      Id = "3f4631bd-f907-4409-b416-ba356312e659",
-                     Email = "user@localhost.com",
-                     NormalizedEmail = "USER@LOCALHOST.COM",
-                     NormalizedUserName = "USER@LOCALHOST.COM",
-                     UserName = "user@localhost.com",
-                     Firstname = "System",
-                     Lastname = "User",
+                     Email = "user1@localhost.com",
+                     NormalizedEmail = "USER1@LOCALHOST.COM",
+                     NormalizedUserName = "USER1@LOCALHOST.COM",
+                     UserName = "user1@localhost.com",
+                     Firstname = "John",
+                     Lastname = "Connor",
+                     PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                     EmailConfirmed = true
+                 },
+                 new Employee
+                 {
+                     Id = "3f4631bd-f907-4410-b416-ba356312e659",
+                     Email = "user2@localhost.com",
+                     NormalizedEmail = "USER2@LOCALHOST.COM",
+                     NormalizedUserName = "USER2@LOCALHOST.COM",
+                     UserName = "user2@localhost.com",
+                     Firstname = "Sarah",
+                     Lastname = "Connor",
+                     PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                     EmailConfirmed = true
+                 },
+                 new Employee
+                 {
+                     Id = "3f4631bd-f907-4411-b416-ba356312e659",
+                     Email = "user3@localhost.com",
+                     NormalizedEmail = "USER3@LOCALHOST.COM",
+                     NormalizedUserName = "USER3@LOCALHOST.COM",
+                     UserName = "user3@localhost.com",
+                     Firstname = "Niki",
+                     Lastname = "Lauda",
+                     PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                     EmailConfirmed = true
+                 },
+                 new Employee
+                 {
+                     Id = "3f4631bd-f907-4412-b416-ba356312e659",
+                     Email = "user4@localhost.com",
+                     NormalizedEmail = "USER4@LOCALHOST.COM",
+                     NormalizedUserName = "USER4@LOCALHOST.COM",
+                     UserName = "user4@localhost.com",
+                     Firstname = "James",
+                     Lastname = "Hunt",
+                     PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                     EmailConfirmed = true
+                 },
+                 new Employee
+                 {
+                     Id = "3f4631bd-f907-4413-b416-ba356312e659",
+                     Email = "user5@localhost.com",
+                     NormalizedEmail = "USER5@LOCALHOST.COM",
+                     NormalizedUserName = "USER5@LOCALHOST.COM",
+                     UserName = "user5@localhost.com",
+                     Firstname = "Mario",
+                     Lastname = "Andretti",
+                     PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                     EmailConfirmed = true
+                 },
+                 new Employee
+                 {
+                     Id = "3f4631bd-f907-4414-b416-ba356312e659",
+                     Email = "user6@localhost.com",
+                     NormalizedEmail = "USER6@LOCALHOST.COM",
+                     NormalizedUserName = "USER6@LOCALHOST.COM",
+                     UserName = "use6r@localhost.com",
+                     Firstname = "Scott",
+                     Lastname = "Bloomquist",
+                     PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                     EmailConfirmed = true
+                 },
+                 new Employee
+                 {
+                     Id = "3f4631bd-f907-4415-b416-ba356312e659",
+                     Email = "user7@localhost.com",
+                     NormalizedEmail = "USER7@LOCALHOST.COM",
+                     NormalizedUserName = "USER7@LOCALHOST.COM",
+                     UserName = "user7@localhost.com",
+                     Firstname = "John",
+                     Lastname = "Force",
+                     PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                     EmailConfirmed = true
+                 },
+                 new Employee
+                 {
+                     Id = "3f4631bd-f907-4416-b416-ba356312e659",
+                     Email = "user8@localhost.com",
+                     NormalizedEmail = "USER8@LOCALHOST.COM",
+                     NormalizedUserName = "USER8@LOCALHOST.COM",
+                     UserName = "user8@localhost.com",
+                     Firstname = "Don",
+                     Lastname = "Prudhomme",
+                     PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                     EmailConfirmed = true
+                 },
+                 new Employee
+                 {
+                     Id = "3f4631bd-f907-4417-b416-ba356312e659",
+                     Email = "user9@localhost.com",
+                     NormalizedEmail = "USER9@LOCALHOST.COM",
+                     NormalizedUserName = "USER9@LOCALHOST.COM",
+                     UserName = "user9@localhost.com",
+                     Firstname = "Tom",
+                     Lastname = "McEwen",
                      PasswordHash = hasher.HashPassword(null, "P@ssword1"),
                      EmailConfirmed = true
                  }
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ The main objective was to track employee leave.
 - Let employees see how much leave they are allocated and leave remaining
 - Allow admin to approve or reject request of leave
 
-Development
+### Development
 - ASP.NET Core
 - Entity Framework Core
 - C# and .Net Core Web Syntax
@@ -18,12 +18,21 @@ Development
 - Manage Packages with NuGet Manager
 - Bootstrap to style and manipulate the overall layout
 - Implemented Website Layout using bootstrap theme
-- Setup GitHub for Source Control
-- 
 
+### App on Azure
 [Live Link](hrmanagementweb20230818105522.azurewebsites.net)
 
 - Login using: Username: admin@localhost.com | Password: P@ssword1
+
+Registration will not work on deployed app.  If you want to test functionality download locally.
+
+### To Configure For Local Use
+Clone Repository To Local Computer
+Open Project in Visual Studio
+In HRManagementWeb/appsettings.json edit "DefaultConnection" to point to preferred database
+Open Package Console Manager and run the command "Update-Database"
+Download Papercut SMPT
+When completed hit Control+F5 to run application
 
 Future Feature
 - Track employee hours 
